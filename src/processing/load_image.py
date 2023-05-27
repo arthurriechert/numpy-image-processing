24611c0776a45ba4ee0a4163e558e64728e09605
Arthur Riechert <riechertarthur@gmail.com>
2023-05-27 04:38:21 -0400

    Added load_image_as_array, display_image_as_array

--- src/processing/load_image.py
+++ src/processing/load_image.py
@@ -5,13 +5,26 @@ import imageio.v3 as iio
 import matplotlib.pyplot as plt
 
 # Used to check if path exists
+import os
+
+# Used to create random array
+import numpy as np
 
 def load_image_as_array(path: str):
 
-    return iio.imread(path)
+    # Make sure the path exists
+    exists = os.path.exists(path)
+
+    if exists:
+        print('Found image.')
+        return iio.imread(path)
+    else:
+        return None
 
 def display_array_image(np_array):
+    print(f'Displaying following array\n\033[32m{np_array}\033[0m')
     plt.imshow(np_array)
+    plt.show()
 
 image = load_image_as_array('../../images/smiling-guy.jpg')
 display_array_image(image)
