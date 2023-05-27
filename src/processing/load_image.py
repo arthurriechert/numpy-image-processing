e2fd461304a286fae301fd9c6887fe29b2e01dab
Arthur Riechert <riechertarthur@gmail.com>
2023-05-27 19:01:19 -0400

    Moved function calls to main and added comments

--- src/processing/load_image.py
+++ src/processing/load_image.py
@@ -10,6 +10,7 @@ import os
 # Used to create random array
 import numpy as np
 
+# Convert image into numpy array || Takes path as a string
 def load_image_as_array(path: str):
 
     # Make sure the path exists
@@ -21,10 +22,8 @@ def load_image_as_array(path: str):
     else:
         return None
 
+# Display image using matplotlib || Takes numpy array
 def display_array_image(np_array):
     print(f'Displaying following array\n\033[32m{np_array}\033[0m')
     plt.imshow(np_array)
     plt.show()
-
-image = load_image_as_array('../../images/smiling-guy.jpg')
-display_array_image(image)
