e756d898b898e11bf9ab89d14f6f8a59af61bfa5
Arthur Riechert <riechertarthur@gmail.com>
2023-05-28 02:20:23 -0400

    Implemented rgb_min_max_linear_stretch by applying formulas from README.md

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import processing.load_image as pr
 import processing.grayscale_conversion as gsc
 import processing.negative_conversion as nc
+import processing.add_contrast as ac
 
 if __name__ == "__main__":
     image_array = pr.load_image_as_array('../images/smiling-guy.jpg')
-    # R, G, B = gsc.matrix_decomposition(image_array)
+    R, G, B = gsc.matrix_decomposition(image_array)
     # image_array = gsc.rgb_to_grayscale(R, G, B)
-    image_array = nc.rgb_to_negative(image_array)
-    print(f'Negative matrix is:\n{image_array}')
+    image_array = ac.rgb_min_max_linear_stretch(R, G, B)
+    # image_array = nc.rgb_to_negative(image_array)
+    print(f'Stretched matrix is:\n{image_array}')
+    print(f'Shape is {image_array.shape}')
     pr.display_array_image(image_array)
--- src/processing/add_contrast.py
+++ src/processing/add_contrast.py
@@ -3,6 +3,23 @@ import numpy as np
 
 # Takes 3D, RGB array and does min-max stretch || Takes numpy array
 # Check README.md for more info
-def rgb_min_max_linear_stretch(np_array):
-    np_array[2].split()
-    print(np_array)
+# g(x, y)=(f(x,y)-min)/(max-min))*No. of the intensity level
+def rgb_min_max_linear_stretch(R, G, B):
+
+    print(f'Input arrays are: \nR:{R.shape}\nG:{G.shape}\nB:{B.shape}\nHere are the input arrays:\n\033[32m{np.transpose(np.array([R, G, B], dtype=np.uint8), (1, 2, 0))}\033[0m')
+
+    # Find minimum and maximum intensity values
+    min_R, max_R = np.min(R), np.max(R)
+    min_G, max_G = np.min(G), np.max(G)
+    min_B, max_B = np.min(B), np.max(B)
+    print(f'Min is {min_R}\nMax is {max_R}')
+
+    # Use numpy built-in broadcasting to apply the formula
+    R = ((R - min_R) / (max_R - min_R)) * 255
+    G = ((G - min_G) / (max_G - min_G)) * 255
+    B = ((B - min_B) / (max_B - min_B)) * 255
+
+    # print(f'Stretched arrays are: \nR:{tmp_R}\nG:{tmp_G}\nB:{tmp_B}')
+
+    # Transpose to ensure correct shape and apply uint8 to avoid incorrect truncation
+    return np.transpose(np.array([R, G, B], dtype=np.uint8), (1, 2, 0))
