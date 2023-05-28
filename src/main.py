8794038b8865c64e408ab65bf7db1d64a65a4dfd
Arthur Riechert <riechertarthur@gmail.com>
2023-05-28 00:00:15 -0400

    Implemented RGB to negative conversion

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import processing.load_image as pr
-import processing.gray_scale_conversion as gsc
+import processing.grayscale_conversion as gsc
+import processing.negative_conversion as nc
 
 if __name__ == "__main__":
     image_array = pr.load_image_as_array('../images/smiling-guy.jpg')
-    R, G, B = gsc.matrix_decomposition(image_array)
-    image_array = gsc.convert_RGB_to_grayscale(R, G, B)
-    print(f'Gray-scaled matrix is:\n{image_array}')
-    pr.display_array_image(image_array, 'gray')
+    # R, G, B = gsc.matrix_decomposition(image_array)
+    # image_array = gsc.rgb_to_grayscale(R, G, B)
+    image_array = nc.rgb_to_negative(image_array)
+    print(f'Negative matrix is:\n{image_array}')
+    pr.display_array_image(image_array)
---
+++ src/processing/negative_conversion.py
@@ -0,0 +1,2 @@
+def rgb_to_negative(np_array):
+    return 255 - np_array
