fb88b089c333f41fe244b1f24ab3041a0bf0dd41
Arthur Riechert <riechertarthur@gmail.com>
2023-05-27 23:36:55 -0400

    Added test for grayscale conversion and it works

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import processing.load_image as pr
+import processing.gray_scale_conversion as gsc
 
 if __name__ == "__main__":
-    image = pr.load_image_as_array('../images/smiling-guy.jpg')
-    pr.display_array_image(image)
+    image_array = pr.load_image_as_array('../images/smiling-guy.jpg')
+    R, G, B = gsc.matrix_decomposition(image_array)
+    image_array = gsc.convert_RGB_to_grayscale(R, G, B)
+    print(f'Gray-scaled matrix is:\n{image_array}')
+    pr.display_array_image(image_array, 'gray')
