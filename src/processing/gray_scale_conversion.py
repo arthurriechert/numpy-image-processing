1727303c326da6a16839cb0a5fc834f0404ff70b
Arthur Riechert <riechertarthur@gmail.com>
2023-05-27 23:36:02 -0400

    Fixed matrix_decomposition and convert_RGB_to_grayscale calculations by adding matrices and correcting np indices

--- src/processing/gray_scale_conversion.py
+++ src/processing/gray_scale_conversion.py
@@ -1,7 +1,17 @@
-import numpy as np
-
+# Convert an RGB matrix into individual components || Takes numpy array
 def matrix_decomposition(np_array):
-    R = np_array[0]
-    G = np_array[1]
-    B = np_array[2]
+    R = np_array[:,:,0]
+    G = np_array[:,:,1]
+    B = np_array[:,:,2]
     return R, G, B
+
+# Converts R, G, B to grayscale || R, G, B are numpy arrays
+def convert_RGB_to_grayscale(R, G, B):
+    R = R * 0.299
+    G = G * 0.587
+    B = B * 0.114
+    
+    # Combine into single array
+    grayscale = R + G + B
+
+    return grayscale
