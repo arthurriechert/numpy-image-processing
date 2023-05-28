603d7e5b4a48b76a9aeb315aa33aa266ef22a5a2
Arthur Riechert <riechertarthur@gmail.com>
2023-05-28 00:36:08 -0400

    Created min_max_linear_stretch

---
+++ src/processing/add_contrast.py
@@ -0,0 +1,8 @@
+# Main library for manipulating image matrix representations
+import numpy as np
+
+# Takes 3D, RGB array and does min-max stretch || Takes numpy array
+# Check README.md for more info
+def rgb_min_max_linear_stretch(np_array):
+    np_array[2].split()
+    print(np_array)
