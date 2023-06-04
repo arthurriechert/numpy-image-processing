7eb14d6788b7ff80733811f6a8fff12d14f880d4
Arthur Riechert <riechertarthur@gmail.com>
2023-06-03 20:42:05 -0400

    Implemented cdf

--- src/processing/histogram_equalization.py
+++ src/processing/histogram_equalization.py
@@ -1,14 +1,42 @@
 import numpy as np
-from grayscale_conversion import rgb_to_grayscale, matrix_decomposition
 
-# Calculate the probability for each pixel's intensity value
+# Count the number of times each intensity value appears
 def compute_histogram(grayscale_np_array):
 
     # Use built-in np function to compute the histogram
     return np.histogram(grayscale_np_array.flatten(), bins=256, range=[0,256])
 
+# Calculate the probabilites for each intensity value
+def compute_probabilties(histogram):
+
+    pixel_count = np.sum(histogram)
+
+    # Normalize
+    return histogram / pixel_count
+
+def compute_cdf(probabilities):
+
+    return np.cumsum(probabilities)
+
 def test(np_array):
 
     histogram,_ = compute_histogram(np_array)
 
-    print(f"Histogram for the Image: {histogram}")
+    probabilties = compute_probabilties(histogram)
+
+    cdf = compute_cdf(probabilties)
+
+    print(f"""
+        ###### HISTOGRAM ######
+          
+        {histogram}
+
+        
+        ###### PROBABILTIIES ######
+
+        {probabilties}
+
+        ###### CDF ######
+
+        {cdf}
+        """)
